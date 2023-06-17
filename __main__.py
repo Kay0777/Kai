5d91d571aaf4bcd510dfb5c86c63ecfa1d1ff179
Kay0777 <ronin.kay0705@gmail.com>
2023-06-17 16:31:47 +0500

    Hi!

--- __main__.py
+++ __main__.py
@@ -1,11 +1,13 @@
 from ai import Kai
 import sys
 
+# https://github.com/nateshmbhat/pyttsx3
+
 
 def main():
   kai = Kai()
-  kai.speak("Fock you bitch!")
-  print("Fock you bitch!")
+  # kai.speak("Blocks while processing all currently queued commands. Invokes callbacks for engine notifications appropriately. Returns when all commands queued before this call are emptied from the queue.")
+  # print("Fock you bitch!")
 
 
 if __name__ == "__main__":
--- ai/voice.py
+++ ai/voice.py
@@ -4,11 +4,21 @@ import pyttsx3
 class KaiVoice:
   def __init__(self):
     self.__engine = pyttsx3.init()
+    self.__voice = pyttsx3.voice.Voice()
 
-    voices = self.__engine.getProperty('voice')
-    # print(voices)
-    # self.__engine.setProperty('voice', voices[1].id)
+    self.__initSettings()
 
-  def speak(self, speache):
-    self.__engine.say(speache)
-    self.__engine.runAndWait()
+  def __initSettings(self):
+    voice1, voice2 = self.__engine.getProperty('voices')
+    self.__engine.setProperty('voice', voice2.id)
+    self.__engine.setProperty('rate', 130)
+
+    print(self.__voice.age)
+
+  def speak(self, speache: str) -> None:
+    if self.isBusy():
+      self.__engine.say(speache)
+      self.__engine.runAndWait()
+
+  def isBusy(self) -> bool:
+    return self.__engine.isBusy()
